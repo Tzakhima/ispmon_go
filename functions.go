b6056cd8f0bfeb85ea580de2a05e51620acb746f
Tzakhi Malka <tzakhi.m@tzakhi-ma-nmbp.office.taboola.com>
2021-01-09 20:50:44 +0200

    Saving

--- functions.go
+++ functions.go
@@ -7,7 +7,6 @@ import (
 	"github.com/ddo/go-fast"
 	"github.com/go-ping/ping"
 	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
 	"net/http/httptrace"
@@ -68,13 +67,13 @@ func getParameters() ([]string, []string, int, error) {
 
 	body, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
-		return nil, nil, -1, fmt.Errorf("reading response body error: %g", readErr)
+		return nil, nil, -1, fmt.Errorf("could not read Body: %g", readErr)
 	}
 
 	response := parameters{}
 	jsonErr := json.Unmarshal(body, &response)
 	if jsonErr != nil {
-		log.Fatal(jsonErr)
+		return nil, nil, -1, fmt.Errorf("could not unmarshal json: %g", jsonErr)
 	}
 
 	return response.Ping, response.HTTP, response.Interval, nil
@@ -88,12 +87,12 @@ func getIspInfo() (map[string]string, error) {
 
 	req, err := http.NewRequest(http.MethodGet, IpInfoUrl, nil)
 	if err != nil {
-		log.Fatal("Setting Req Error: ",err)
+		return nil, fmt.Errorf("could not build HTTP request: %g", err)
 	}
 
 	res, getErr := infoClient.Do(req)
 	if getErr != nil {
-		log.Fatal("Do req Error: ",getErr)
+		return nil, fmt.Errorf("could not get HTTP request: %g", getErr)
 	}
 
 	if res.Body != nil {
@@ -102,13 +101,13 @@ func getIspInfo() (map[string]string, error) {
 
 	body, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
-		log.Fatal("Read Error: ",readErr)
+		return nil, fmt.Errorf("could not read body: %g", readErr)
 	}
 
 	respInfo := ipInfoStruct{}
 	jsonErr  := json.Unmarshal(body, &respInfo)
 	if jsonErr != nil {
-		log.Fatal("JSON Unmarshal Error: ",jsonErr)
+		return nil, fmt.Errorf("could not unmarshal JSON: %g", jsonErr)
 	}
 
 	returnInfo := make(map[string]string)
@@ -156,7 +155,7 @@ func getHttpStat(url string, c chan map[string]map[string]int64) {
 	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
 	start = time.Now()
 	if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
-		log.Fatal(err)
+		fmt.Errorf("could not run HTTP stat: %g", err)
 	}
 
 	c <- results
--- main.go
+++ main.go
@@ -13,7 +13,9 @@ package main
 import (
 	"crypto/sha1"
 	"fmt"
+	"os"
 	"sync"
+	"time"
 )
 
 const (
@@ -25,7 +27,7 @@ const (
 type pushResults struct {
 	http		[]map[string]map[string]int64
 	ping		[]map[string]map[string]float64
-	speed       float64
+	speed       string
 	uid			string
 	isp 		string
 	country		string
@@ -34,11 +36,15 @@ type pushResults struct {
 
 func main() {
 
+	// Getting current time for Speedtest interval
+	start := time.Now()
+
 	// calculate user unique id based on MAC addresses. variable: uid
     mac, err := getMacAddr()
 
     if err != nil {
     	fmt.Printf("Error getting Mac address: %s", err )
+    	os.Exit(1) // If we cant generate UID there is no pint to continue
 	}
 
 	var macString string
@@ -51,61 +57,73 @@ func main() {
 
 
 	// get client and ISP info from ipinfo.io
-	IpInfo, err := getIspInfo()
-	if err !=nil {
-		fmt.Printf("Error getting client IP info: %s", err )
+	GETINFO: // yes yes I know, goto is ugly...
+		IpInfo, err := getIspInfo()
+		if err !=nil {
+			fmt.Printf("Error getting client IP info: %s", err )
+			fmt.Println("Trying again in 2 sec")
+			time.Sleep(2*time.Second)
+			goto GETINFO
+		}
+
+    for {
+		// getting targets and interval parameters
+    	GETPARAM:
+			pingLinks, httpLinks, interval, err := getParameters()
+			if err != nil {
+				fmt.Printf("Error getting parameters: %s", err )
+				fmt.Println("Trying again in 2 sec")
+				goto GETPARAM
+			}
+
+
+		// http trace
+		var httpResults []map[string]map[string]int64
+		c := make(chan map[string]map[string]int64) // Using channel and not WaitingGroup just for fun :-)
+
+		for _, link := range httpLinks {
+			go getHttpStat(link, c)
+		}
+
+		for i := 0; i < len(httpLinks); i++ {
+			httpResults = append(httpResults, <-c)
+		}
+
+		// PING test
+		var wg sync.WaitGroup
+		var pingResults []map[string]map[string]float64
+
+		for _, t := range pingLinks {
+			wg.Add(1)
+			go func(t string) {
+				result := getPingStat(t, &wg)
+				pingResults = append(pingResults, result)
+			}(t)
+		}
+
+		wg.Wait()
+
+		// speed test
+		downloadSpeed := "null"
+		t := time.Now()
+		elapsed := t.Sub(start)
+		if int(elapsed) >= interval {
+			downloadSpeed = fmt.Sprintf("%f", getDownloadSpeed()) //float64 to String
+			start = time.Now()
+		}
+
+		// push results
+		push := pushResults{}
+		push.speed = downloadSpeed
+		push.http = httpResults
+		push.ping = pingResults
+		push.isp = IpInfo["isp"]
+		push.country = IpInfo["country"]
+		push.city = IpInfo["city"]
+		push.uid = uid
+
+		fmt.Printf("%+v\n", push)
 	}
-
-
-	// getting targets and interval parameters
-	pingLinks, httpLinks, interval, err := getParameters()
-	fmt.Print(interval)
-
-
-	// http trace
-	var httpResults []map[string]map[string]int64
-	c := make(chan map[string]map[string]int64) // Using channel and not WaitingGroup just for fun :-)
-
-	for _, link := range httpLinks {
-		go getHttpStat(link, c)
-	}
-
-	for i := 0; i < len(httpLinks); i++ {
-		httpResults = append(httpResults, <-c)
-	}
-
-
-	// PING test
-	var wg sync.WaitGroup
-    var pingResults []map[string]map[string]float64
-
-	for _, t := range pingLinks {
-		wg.Add(1)
-		go func (t string){
-			result := getPingStat(t, &wg)
-			pingResults = append(pingResults, result)
-		}(t)
-	}
-
-	wg.Wait()
-
-
-	// speed test
-    var downloadSpeed float64
-	downloadSpeed = getDownloadSpeed()
-
-
-	// push results
-	push := pushResults{}
-	push.speed   = downloadSpeed
-	push.http    = httpResults
-	push.ping    = pingResults
-	push.isp     = IpInfo["isp"]
-	push.country = IpInfo["country"]
-	push.city    = IpInfo["city"]
-	push.uid     = uid
-
-	fmt.Printf("%+v\n", push)
 
 
 }
