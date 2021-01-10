81fb5a0101bead11ddcfce3838018105425682ed
Tzakhi Malka <tzakhi.m@tzakhi-ma-nmbp.office.taboola.com>
2021-01-10 14:40:46 +0200

    Formatting

--- functions.go
+++ functions.go
@@ -1,238 +1,238 @@
 package main
 
 import (
-	"crypto/tls"
-	"encoding/json"
-	"fmt"
-	"github.com/ddo/go-fast"
-	"github.com/go-ping/ping"
-	"io/ioutil"
-	"net"
-	"net/http"
-	"net/http/httptrace"
-	"sync"
-	"time"
+    "crypto/tls"
+    "encoding/json"
+    "fmt"
+    "github.com/ddo/go-fast"
+    "github.com/go-ping/ping"
+    "io/ioutil"
+    "net"
+    "net/http"
+    "net/http/httptrace"
+    "sync"
+    "time"
 )
 
 // Init global types
 type parameters struct {
-	Ping		[]string `json:"ping_target"`
-	HTTP      	[]string `json:"http_target"`
-	Interval  	int      `json:"speed_interval"`
+    Ping        []string `json:"ping_target"`
+    HTTP          []string `json:"http_target"`
+    Interval      int      `json:"speed_interval"`
 }
 
 type ipInfoStruct struct {
-	City		string  `json:"city"`
-    Country		string  `json:"country"`
-	ISP			string  `json:"org"`
+    City        string  `json:"city"`
+    Country        string  `json:"country"`
+    ISP            string  `json:"org"`
 }
 
 
 // Functions Start
 func getMacAddr() ([]string, error) {
-	ifas, err := net.Interfaces()
-	if err != nil {
-		return nil, err
-	}
-	var as []string
-	for _, ifa := range ifas {
-		a := ifa.HardwareAddr.String()
-		if a != "" {
-			as = append(as, a)
-		}
-	}
-	return as, nil
+    ifas, err := net.Interfaces()
+    if err != nil {
+        return nil, err
+    }
+    var as []string
+    for _, ifa := range ifas {
+        a := ifa.HardwareAddr.String()
+        if a != "" {
+            as = append(as, a)
+        }
+    }
+    return as, nil
 }
 
 func getParameters() ([]string, []string, int, error) {
 
-	apiClient := http.Client{
-		Timeout: time.Second * 2,
-	}
+    apiClient := http.Client{
+        Timeout: time.Second * 2,
+    }
 
-	req, err := http.NewRequest(http.MethodGet, ParametersUrl, nil)
-	if err != nil {
-		return nil, nil, -1, fmt.Errorf("could not build HTTP request: %g", err)
-	}
+    req, err := http.NewRequest(http.MethodGet, ParametersUrl, nil)
+    if err != nil {
+        return nil, nil, -1, fmt.Errorf("could not build HTTP request: %g", err)
+    }
 
-	res, getErr := apiClient.Do(req)
+    res, getErr := apiClient.Do(req)
 
-	if getErr != nil {
-		return nil, nil, -1, fmt.Errorf("could not get HTTP request: %g", getErr)
-	}
+    if getErr != nil {
+        return nil, nil, -1, fmt.Errorf("could not get HTTP request: %g", getErr)
+    }
 
-	if res.Body != nil {
-		defer res.Body.Close()
-	}
+    if res.Body != nil {
+        defer res.Body.Close()
+    }
 
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		return nil, nil, -1, fmt.Errorf("could not read Body: %g", readErr)
-	}
+    body, readErr := ioutil.ReadAll(res.Body)
+    if readErr != nil {
+        return nil, nil, -1, fmt.Errorf("could not read Body: %g", readErr)
+    }
 
-	response := parameters{}
-	jsonErr := json.Unmarshal(body, &response)
-	if jsonErr != nil {
-		return nil, nil, -1, fmt.Errorf("could not unmarshal json: %g", jsonErr)
-	}
+    response := parameters{}
+    jsonErr := json.Unmarshal(body, &response)
+    if jsonErr != nil {
+        return nil, nil, -1, fmt.Errorf("could not unmarshal json: %g", jsonErr)
+    }
 
-	return response.Ping, response.HTTP, response.Interval, nil
+    return response.Ping, response.HTTP, response.Interval, nil
 
 }
 
 func getIspInfo() (map[string]string, error) {
-	infoClient := http.Client{
-		Timeout: time.Second * 2,
-	}
+    infoClient := http.Client{
+        Timeout: time.Second * 2,
+    }
 
-	req, err := http.NewRequest(http.MethodGet, IpInfoUrl, nil)
-	if err != nil {
-		return nil, fmt.Errorf("could not build HTTP request: %g", err)
-	}
+    req, err := http.NewRequest(http.MethodGet, IpInfoUrl, nil)
+    if err != nil {
+        return nil, fmt.Errorf("could not build HTTP request: %g", err)
+    }
 
-	res, getErr := infoClient.Do(req)
-	if getErr != nil {
-		return nil, fmt.Errorf("could not get HTTP request: %g", getErr)
-	}
+    res, getErr := infoClient.Do(req)
+    if getErr != nil {
+        return nil, fmt.Errorf("could not get HTTP request: %g", getErr)
+    }
 
-	if res.Body != nil {
-		defer res.Body.Close()
-	}
+    if res.Body != nil {
+        defer res.Body.Close()
+    }
 
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		return nil, fmt.Errorf("could not read body: %g", readErr)
-	}
+    body, readErr := ioutil.ReadAll(res.Body)
+    if readErr != nil {
+        return nil, fmt.Errorf("could not read body: %g", readErr)
+    }
 
-	respInfo := ipInfoStruct{}
-	jsonErr  := json.Unmarshal(body, &respInfo)
-	if jsonErr != nil {
-		return nil, fmt.Errorf("could not unmarshal JSON: %g", jsonErr)
-	}
+    respInfo := ipInfoStruct{}
+    jsonErr  := json.Unmarshal(body, &respInfo)
+    if jsonErr != nil {
+        return nil, fmt.Errorf("could not unmarshal JSON: %g", jsonErr)
+    }
 
-	returnInfo := make(map[string]string)
-	returnInfo["country"] = respInfo.Country
-	returnInfo["city"]    = respInfo.City
-	returnInfo["isp"]     = respInfo.ISP
+    returnInfo := make(map[string]string)
+    returnInfo["country"] = respInfo.Country
+    returnInfo["city"]    = respInfo.City
+    returnInfo["isp"]     = respInfo.ISP
 
-	return returnInfo, nil
+    return returnInfo, nil
 
 }
 
 func getHttpStat(url string, c chan map[string]map[string]int64) {
 
-	schema := "https://"
+    schema := "https://"
 
-	req, _ := http.NewRequest("GET", schema+url, nil)
+    req, _ := http.NewRequest("GET", schema+url, nil)
 
-	var start, connect, dns, tlsHandshake time.Time
+    var start, connect, dns, tlsHandshake time.Time
 
     results := make(map[string]map[string]int64)
-	results[url] = make(map[string]int64)
+    results[url] = make(map[string]int64)
 
 
-	trace := &httptrace.ClientTrace{
-		DNSStart: func(dsi httptrace.DNSStartInfo) { dns = time.Now() },
-		DNSDone: func(ddi httptrace.DNSDoneInfo) {
-			results[url]["dnsTime"] = int64(time.Since(dns) / time.Millisecond)
-		},
+    trace := &httptrace.ClientTrace{
+        DNSStart: func(dsi httptrace.DNSStartInfo) { dns = time.Now() },
+        DNSDone: func(ddi httptrace.DNSDoneInfo) {
+            results[url]["dnsTime"] = int64(time.Since(dns) / time.Millisecond)
+        },
 
-		TLSHandshakeStart: func() { tlsHandshake = time.Now() },
-		TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
-			results[url]["tlsTime"] = int64(time.Since(tlsHandshake) / time.Millisecond)
-		},
+        TLSHandshakeStart: func() { tlsHandshake = time.Now() },
+        TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
+            results[url]["tlsTime"] = int64(time.Since(tlsHandshake) / time.Millisecond)
+        },
 
-		ConnectStart: func(network, addr string) { connect = time.Now() },
-		ConnectDone: func(network, addr string, err error) {
-			results[url]["connTime"] =int64(time.Since(connect) / time.Millisecond)
-		},
+        ConnectStart: func(network, addr string) { connect = time.Now() },
+        ConnectDone: func(network, addr string, err error) {
+            results[url]["connTime"] =int64(time.Since(connect) / time.Millisecond)
+        },
 
-		GotFirstResponseByte: func() {
-			results[url]["ttfbTime"] = int64(time.Since(start) / time.Millisecond)
-		},
-	}
+        GotFirstResponseByte: func() {
+            results[url]["ttfbTime"] = int64(time.Since(start) / time.Millisecond)
+        },
+    }
 
-	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
-	start = time.Now()
-	if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
-		fmt.Errorf("could not run HTTP stat: %g", err)
-	}
+    req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+    start = time.Now()
+    if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
+        fmt.Errorf("could not run HTTP stat: %g", err)
+    }
 
-	c <- results
+    c <- results
 }
 
 func getPingStat(target string, wg *sync.WaitGroup) map[string]map[string]float64 {
-	defer wg.Done()
+    defer wg.Done()
 
-	result := make(map[string]map[string]float64)
-	result[target] = make(map[string]float64)
+    result := make(map[string]map[string]float64)
+    result[target] = make(map[string]float64)
 
-	pinger, err := ping.NewPinger(target)
-	pinger.SetPrivileged(true)
+    pinger, err := ping.NewPinger(target)
+    pinger.SetPrivileged(true)
 
-	if err != nil {
-		fmt.Printf("%+v", err)
-		result[target]["packetLoss"] = 0
-		result[target]["minRTT"]     = 0
-		result[target]["avgRTT"]     = 0
-		result[target]["maxRTT"]     = 0
+    if err != nil {
+        fmt.Printf("%+v", err)
+        result[target]["packetLoss"] = 0
+        result[target]["minRTT"]     = 0
+        result[target]["avgRTT"]     = 0
+        result[target]["maxRTT"]     = 0
 
-		return result
-	}
+        return result
+    }
 
-	pinger.Count = pingCount
-	err = pinger.Run()
-	if err != nil {
-		fmt.Printf("%+v", err)
-		result[target]["packetLoss"] = 0
-		result[target]["minRTT"]     = 0
-		result[target]["avgRTT"]     = 0
-		result[target]["maxRTT"]     = 0
+    pinger.Count = pingCount
+    err = pinger.Run()
+    if err != nil {
+        fmt.Printf("%+v", err)
+        result[target]["packetLoss"] = 0
+        result[target]["minRTT"]     = 0
+        result[target]["avgRTT"]     = 0
+        result[target]["maxRTT"]     = 0
 
-		return result
-	}
+        return result
+    }
 
-	stats := pinger.Statistics()
+    stats := pinger.Statistics()
 
-	result[target]["packetLoss"] = stats.PacketLoss
-	result[target]["minRTT"]     = float64(stats.MinRtt / time.Millisecond)
-	result[target]["avgRTT"]     = float64(stats.AvgRtt / time.Millisecond)
-	result[target]["maxRTT"]     = float64(stats.MaxRtt / time.Millisecond)
+    result[target]["packetLoss"] = stats.PacketLoss
+    result[target]["minRTT"]     = float64(stats.MinRtt / time.Millisecond)
+    result[target]["avgRTT"]     = float64(stats.AvgRtt / time.Millisecond)
+    result[target]["maxRTT"]     = float64(stats.MaxRtt / time.Millisecond)
 
-	return result
+    return result
 
 }
 
 func getDownloadSpeed() float64 {
-	fastCom := fast.New()
+    fastCom := fast.New()
 
-	// init
-	err := fastCom.Init()
-	if err != nil {
-		return 0
-	}
+    // init
+    err := fastCom.Init()
+    if err != nil {
+        return 0
+    }
 
-	// get urls
-	urls, err := fastCom.GetUrls()
-	if err != nil {
-		return 0
-	}
+    // get urls
+    urls, err := fastCom.GetUrls()
+    if err != nil {
+        return 0
+    }
 
-	// measure
-	KbpsChan := make(chan float64)
-	var last float64
+    // measure
+    KbpsChan := make(chan float64)
+    var last float64
 
-	go func() {
-		for Kbps := range KbpsChan {
-			last = Kbps
-		}
-	}()
+    go func() {
+        for Kbps := range KbpsChan {
+            last = Kbps
+        }
+    }()
 
-	err = fastCom.Measure(urls, KbpsChan)
+    err = fastCom.Measure(urls, KbpsChan)
 
-	if err != nil {
-		return 0
-	}
+    if err != nil {
+        return 0
+    }
 
-	return last // return download speed in Kbps
+    return last // return download speed in Kbps
 }
--- main.go
+++ main.go
@@ -11,142 +11,143 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha1"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-	"sync"
-	"time"
+    "bytes"
+    "crypto/sha1"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+    "sync"
+    "time"
 )
 
 const (
-	ParametersUrl string = "http://ispmon.cloud/config"
-	IpInfoUrl     string = "http://ipinfo.io/"
-	PushInfoUrl   string = "http://ispmon.cloud/gometrics"
+    ParametersUrl string = "http://ispmon.cloud/config"
+    IpInfoUrl     string = "http://ipinfo.io/"
+    PushInfoUrl   string = "http://ispmon.cloud/gometrics"
 )
 
+// How many PING to send
 var (
-	pingCount int = 10
+    pingCount int = 10
 )
 
 type PushResults struct {
-	Http		[]map[string]map[string]int64
-	Ping		[]map[string]map[string]float64
-	Speed       string
-	Uid			string
-	Isp 		string
-	Country		string
-	City		string
+    Http        []map[string]map[string]int64
+    Ping        []map[string]map[string]float64
+    Speed        string
+    Uid            string
+    Isp         string
+    Country        string
+    City        string
 }
 
 
 func main() {
 
-	// Getting current time for Speedtest interval
-	start := time.Now()
+    // Getting current time for Speedtest interval
+    start := time.Now()
 
-	// calculate user unique id based on MAC addresses. variable: uid
+    // calculate user unique id based on MAC addresses. variable: uid
     mac, err := getMacAddr()
 
     if err != nil {
-    	fmt.Printf("Error getting Mac address: %s", err )
-    	os.Exit(1) // If we cant generate UID there is no pint to continue
-	}
+        fmt.Printf("Error getting Mac address: %s", err )
+        os.Exit(1) // If we cant generate UID there is no pint to continue
+    }
 
-	var macString string
+    var macString string
     for _, addr := range mac{
-    	macString = macString + addr
-	}
+        macString = macString + addr
+    }
 
-	macSha1  := sha1.Sum([]byte(macString))
+    macSha1  := sha1.Sum([]byte(macString))
     uid := fmt.Sprintf("%x\n", string(macSha1[:]))[0:10]
 
 
-	// get client and ISP info from ipinfo.io
-	GETINFO: // yes yes I know, goto is ugly...
-		IpInfo, err := getIspInfo()
-		if err !=nil {
-			fmt.Printf("Error getting client IP info: %s", err )
-			fmt.Println("Trying again in 2 sec")
-			time.Sleep(2*time.Second)
-			goto GETINFO
-		}
+    // get client and ISP info from ipinfo.io
+    GETINFO: // yes yes I know, goto is ugly...
+        IpInfo, err := getIspInfo()
+        if err !=nil {
+            fmt.Printf("Error getting client IP info: %s", err )
+            fmt.Println("Trying again in 2 sec")
+            time.Sleep(2*time.Second)
+            goto GETINFO
+        }
 
     for {
-		// getting targets and interval parameters
-    	GETPARAM:
-			pingLinks, httpLinks, interval, err := getParameters()
-			if err != nil {
-				fmt.Printf("Error getting parameters: %s", err )
-				fmt.Println("Trying again in 2 sec")
-				goto GETPARAM
-			}
+        // getting targets and interval parameters
+        GETPARAM:
+            pingLinks, httpLinks, interval, err := getParameters()
+            if err != nil {
+                fmt.Printf("Error getting parameters: %s", err )
+                fmt.Println("Trying again in 2 sec")
+                goto GETPARAM
+            }
 
 
-		// http trace
-		var httpResults []map[string]map[string]int64
-		c := make(chan map[string]map[string]int64) // Using channel and not WaitingGroup just for fun :-)
+        // http trace
+        var httpResults []map[string]map[string]int64
+        c := make(chan map[string]map[string]int64) // Using channel and not WaitingGroup just for fun :-)
 
-		for _, link := range httpLinks {
-			go getHttpStat(link, c)
-		}
+        for _, link := range httpLinks {
+            go getHttpStat(link, c)
+        }
 
-		for i := 0; i < len(httpLinks); i++ {
-			httpResults = append(httpResults, <-c)
-		}
+        for i := 0; i < len(httpLinks); i++ {
+            httpResults = append(httpResults, <-c)
+        }
 
-		// PING test
-		var wg sync.WaitGroup
-		var pingResults []map[string]map[string]float64
+        // PING test
+        var wg sync.WaitGroup
+        var pingResults []map[string]map[string]float64
 
-		for _, t := range pingLinks {
-			wg.Add(1)
-			go func(t string) {
-				result := getPingStat(t, &wg)
-				pingResults = append(pingResults, result)
-			}(t)
-		}
+        for _, t := range pingLinks {
+            wg.Add(1)
+            go func(t string) {
+                result := getPingStat(t, &wg)
+                pingResults = append(pingResults, result)
+            }(t)
+        }
 
-		wg.Wait()
+        wg.Wait()
 
-		// speed test
-		downloadSpeed := "null"
-		t := time.Now()
-		elapsed := t.Sub(start).Minutes()
-		if int(elapsed) >= interval {
-			downloadSpeed = fmt.Sprintf("%f", getDownloadSpeed()) //float64 to String
-			start = time.Now()
-		}
+        // speed test
+        downloadSpeed := "null"
+        t := time.Now()
+        elapsed := t.Sub(start).Minutes()
+        if int(elapsed) >= interval {
+            downloadSpeed = fmt.Sprintf("%f", getDownloadSpeed()) //float64 to String
+            start = time.Now()
+        }
 
-		// push results
-		push := PushResults{}
-		push.Speed = downloadSpeed
-		push.Http = httpResults
-		push.Ping = pingResults
-		push.Isp = IpInfo["isp"]
-		push.Country = IpInfo["country"]
-		push.City = IpInfo["city"]
-		push.Uid = uid
+        // push results
+        push := PushResults{}
+        push.Speed = downloadSpeed
+        push.Http = httpResults
+        push.Ping = pingResults
+        push.Isp = IpInfo["isp"]
+        push.Country = IpInfo["country"]
+        push.City = IpInfo["city"]
+        push.Uid = uid
 
-		b, err := json.Marshal(push)
-		req, err := http.NewRequest("POST", PushInfoUrl, bytes.NewBuffer(b))
-		req.Header.Set("Content-Type", "application/json")
+        b, err := json.Marshal(push)
+        req, err := http.NewRequest("POST", PushInfoUrl, bytes.NewBuffer(b))
+        req.Header.Set("Content-Type", "application/json")
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Errorf("could not send HTTP POST: %g", err)
-		}
-		defer resp.Body.Close()
+        client := &http.Client{}
+        resp, err := client.Do(req)
+        if err != nil {
+            fmt.Errorf("could not send HTTP POST: %g", err)
+        }
+        defer resp.Body.Close()
 
-		// Print Response
-		//fmt.Println("response Status:", resp.Status)
-		//fmt.Println("response Headers:", resp.Header)
-		//body, _ := ioutil.ReadAll(resp.Body)
-		//fmt.Println("response Body:", string(body))
-	}
+        // Print Response
+        //fmt.Println("response Status:", resp.Status)
+        //fmt.Println("response Headers:", resp.Header)
+        //body, _ := ioutil.ReadAll(resp.Body)
+        //fmt.Println("response Body:", string(body))
+    }
 
 
 }
